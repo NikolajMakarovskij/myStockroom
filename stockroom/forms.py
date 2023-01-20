d8b6822ebd672b8163236bed60eab4026211ffac
NikolajMakarovskij <adrameleh666gmail.com>
2023-01-20 16:18:56 +0500

    add_labels_at_form

--- stockroom/forms.py
+++ stockroom/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 from consumables.models import Cartridge
-cartridge_score = 5
-CARTRIDGE_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, cartridge_score)]
+cartridge_score = 10
+CARTRIDGE_QUANTITY_CHOICES = [(i, str(i)) for i in range(-10, cartridge_score)]
 
 
 class StockAddCartridgeForm(forms.Form):
-    quantity = forms.TypedChoiceField(choices=CARTRIDGE_QUANTITY_CHOICES, coerce=int)
-    update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
+    quantity = forms.TypedChoiceField(choices=CARTRIDGE_QUANTITY_CHOICES, coerce=int, label='Количество' )
+    update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput, label='Обновить')
