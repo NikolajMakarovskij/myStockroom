bc94110116c1a6e6eca0d078786f6e39032b0cca
NikolajMakarovskij <adrameleh666gmail.com>
2023-02-17 17:44:03 +0500

    stock_methods_complete

--- stockroom/models.py
+++ stockroom/models.py
@@ -1,3 +1,71 @@
+import uuid
 from django.db import models
+from django.urls import reverse
+from consumables.models import Consumables
+from catalog.utils import ModelMixin
 
-# Create your models here.
+class Stockroom (ModelMixin, models.Model):
+    consumables = models.OneToOneField(
+        Consumables,
+        on_delete = models.CASCADE,
+        primary_key = True,
+        help_text="Введите название расходника",
+        verbose_name="Расходники"
+        )
+    categories = models.ForeignKey(
+        'Categories',
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        help_text="Укажите группу",
+        verbose_name="группа"
+        )
+    dateAddToStock = models.DateField(
+        null=True, blank=True,
+        verbose_name="Дата поступления на склад"
+        )
+    dateInstall = models.DateField(
+        null=True, blank=True,
+        verbose_name="Дата установки"
+        )
+    def __str__(self):
+        return self.consumables
+
+    def get_absolute_url(self):
+        return reverse('stockroom:stock-detail', args=[str(self.id)])
+
+    class Meta:
+        verbose_name = 'Кассета'
+        verbose_name_plural = 'Кассеты'
+        ordering = ['consumables']
+
+class Categories(ModelMixin, models.Model):
+    """
+    Модель группы для расходников
+    """
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        help_text="ID"
+        )
+    name = models.CharField(
+        max_length=50,
+        help_text="Введите название",
+        verbose_name="Название"
+        )
+    slug = models.SlugField(
+        max_length=50, unique=True, db_index=True,
+        help_text="Введите URL (для работы навигациии в расходниках)",
+        verbose_name="URL"
+        )
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('stockroom:category',kwargs={'category_slug': self.slug})
+
+
+    class Meta:
+        verbose_name = 'Группа расходников'
+        verbose_name_plural = 'Группы расходников'
+        ordering = ['name']
--- stockroom/views.py
+++ stockroom/views.py
@@ -1,14 +1,58 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404
 from django.views.decorators.http import require_POST
-from consumables.models import Consumables #Cartridge, Toner, Fotoval, Accumulator, Storage
+from consumables.models import Consumables, Categories
+from django.views import generic
+from django.db.models import Q
 from .stock import Stock
 from .forms import StockAddForm, ConsumableInstallForm
-from catalog.utils import menu
+from .models import Stockroom
+from catalog.utils import DataMixin
 
 
-def stock(request):
-    stock = Stock(request)
-    return render(request, 'stock/stock_detail.html', {'stock': stock, 'menu':menu} )
+#Расходники
+class stockroomView(DataMixin, generic.ListView):
+    template_name = 'stock/stock_list.html'
+    model = Stockroom
+    menu_categories = Categories.objects.all()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        menu_categories = Categories.objects.all()
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Склад", searchlink='stockroom:stock_list', menu_categories=menu_categories)
+        context = dict(list(context.items()) + list(c_def.items()))
+        return context
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if not query :
+            query = '' 
+        object_list = Stockroom.objects.filter(
+                Q(consumables__name__icontains=query) | 
+                Q(consumables__manufacturer__name__icontains=query) |
+                Q(consumables__categories__name__icontains=query) |
+                Q(consumables__buhCode__icontains=query) |
+                Q(consumables__score__icontains=query) |
+                Q(consumables__serial__icontains=query) |
+                Q(consumables__invent__icontains=query) |
+                Q(dateInstall__icontains=query) |
+                Q(dateAddToStock__icontains=query) 
+        )
+        return object_list
+
+class stockroomCategoriesView(DataMixin, generic.ListView):
+    template_name = 'stock/stock_list.html'
+    model = Stockroom
+    
+    def get_context_data(self, *, object_list=None, **kwargs ):
+        menu_categories = Categories.objects.all()
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Склад", searchlink='stockroom:stock_list', menu_categories=menu_categories)
+        context = dict(list(context.items()) + list(c_def.items()))
+        return context
+
+    def get_queryset(self):
+        object_list = Stockroom.objects.filter(categories__slug=self.kwargs['category_slug'])
+        return object_list        
 
 
 @require_POST
@@ -24,26 +68,26 @@ def stock_add_consumable(request, consumable_id):
                  number_rack=cd['number_rack'],
                  number_shelf=cd['number_shelf'],
                  )
-    return redirect('stockroom:stock')
+    return redirect('stockroom:stock_list')
 
 def stock_remove_consumable(request, consumable_id):
     stock = Stock(request)
     consumable = get_object_or_404(Consumables, id=consumable_id)
     stock.remove_consumable(consumable)
-    return redirect('stockroom:stock')
+    return redirect('stockroom:stock_list')
 
 @require_POST
-def printer_add_consumable(request, consumable_id):
+def device_add_consumable(request, consumable_id):
     stock = Stock(request)
     consumable = get_object_or_404(Consumables, id=consumable_id)
     form = ConsumableInstallForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
-        stock.printer_install_consumable(consumable=consumable,
+        stock.device_add_consumable(consumable=consumable,
                  quantity=cd['quantity'],
                  update_quantity=cd['update']
                  )
-    return redirect('stockroom:stock')
+    return redirect('stockroom:stock_list')
 
 
 @require_POST
@@ -57,7 +101,7 @@ def printer_add_cartridge(request, cartridge_id):
                  quantity=cd['quantity'],
                  update_quantity=cd['update']
                  )
-    return redirect('stockroom:stock')
+    return redirect('stockroom:stock_list')
 
 
 @require_POST
@@ -71,7 +115,7 @@ def printer_add_toner(request, toner_id):
                  quantity=cd['quantity'],
                  update_quantity=cd['update']
                  )
-    return redirect('stockroom:stock')
+    return redirect('stockroom:stock_list')
 
 
 @require_POST
@@ -85,7 +129,7 @@ def printer_add_fotoval(request, fotoval_id):
                  quantity=cd['quantity'],
                  update_quantity=cd['update']
                  )
-    return redirect('stockroom:stock')
+    return redirect('stockroom:stock_list')
 
 @require_POST
 def printer_add_fotodrumm(request, fotodrumm_id):
@@ -98,7 +142,7 @@ def printer_add_fotodrumm(request, fotodrumm_id):
                  quantity=cd['quantity'],
                  update_quantity=cd['update']
                  )
-    return redirect('stockroom:stock')
+    return redirect('stockroom:stock_list')
 
 
 @require_POST
@@ -112,7 +156,7 @@ def ups_add_accumulator(request, accumulator_id):
                  quantity=cd['quantity'],
                  update_quantity=cd['update']
                  )
-    return redirect('stockroom:stock_detail')
+    return redirect('stockroom:stock_list')
 
 
 @require_POST
@@ -126,6 +170,6 @@ def add_storage(request, storage_id):
                  quantity=cd['quantity'],
                  update_quantity=cd['update']
                  )
-    return redirect('stockroom:stock_detail')
+    return redirect('stockroom:stock_list')
 
 
