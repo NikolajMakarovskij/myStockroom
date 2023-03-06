c19a48470650c5c0b3e9806ec74c23b111c1f19f
NikolajMakarovskij <adrameleh666@gmail.com>
2023-03-06 20:18:55 +0500

    fix_tests

--- consumables/tests/test_forms.py
+++ consumables/tests/test_forms.py
@@ -38,7 +38,7 @@ def test_consumable_form_code_invalid():
     err_mes = "Обязательное поле."
     form_data = {
         'name': "my_consumable",
-        'buhCode': "2",
+        'buhCode': "",
     }
     form = consumablesForm(data=form_data)
     assert form.is_valid() is False
