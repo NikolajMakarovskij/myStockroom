e48178bf9af6230e875820c01f8884901c0519e0
refactor_catalog_pep8 <adrameleh666@gmail.com>
2023-08-10 09:06:21 +0500

    fix errors in search select2widget in device form, add device searsh in stock app

--- backend/stockroom/views/accessories.py
+++ backend/stockroom/views/accessories.py
@@ -36,6 +36,9 @@ class StockAccView(LoginRequiredMixin, DataMixin, generic.ListView):
             query = ''
         object_list = StockAcc.objects.filter(
             Q(stock_model__name__icontains=query) |
+            Q(stock_model__device__name__icontains=query) |
+            Q(stock_model__device__workplace__name__icontains=query) |
+            Q(stock_model__device__workplace__room__name__icontains=query) |
             Q(stock_model__manufacturer__name__icontains=query) |
             Q(stock_model__categories__name__icontains=query) |
             Q(stock_model__quantity__icontains=query) |
