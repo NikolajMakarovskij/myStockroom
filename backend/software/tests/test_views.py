b5505dabb06c8fe320a871ffc6f887cfbcc0de20
NikolajMakarovskij <adrameleh666@gmail.com>
2023-08-27 15:43:42 +0500

    add permissions in software app

--- backend/software/tests/test_views.py
+++ backend/software/tests/test_views.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 class SoftwareViewTest(TestCase):
     def setUp(self):
         self.client = Client()
-        self.client.force_login(User.objects.get_or_create(username='user')[0])
+        self.client.force_login(User.objects.get_or_create(username='user', is_superuser=True, is_staff=True)[0])
 
     @classmethod
     def setUpTestData(cls):
@@ -67,7 +67,7 @@ class SoftwareViewTest(TestCase):
 class OSViewTest(TestCase):
     def setUp(self):
         self.client = Client()
-        self.client.force_login(User.objects.get_or_create(username='user')[0])
+        self.client.force_login(User.objects.get_or_create(username='user', is_superuser=True, is_staff=True)[0])
 
     @classmethod
     def setUpTestData(cls):
