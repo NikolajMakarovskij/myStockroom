1bc2d59d7b5cefb09b9517cc4afd4e2abb7d51b5
NikolajMakarovskij <adrameleh666@gmail.com>
2023-11-14 14:55:59 +0500

    add import-export plugin to admin panel in stock app

--- backend/stockroom/admin.py
+++ backend/stockroom/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from import_export.admin import ImportExportModelAdmin
 from core.utils import ExportAdmin
 from stockroom.models.accessories import CategoryAcc, HistoryAcc, StockAcc
 from stockroom.models.consumables import StockCat, History, Stockroom
@@ -7,154 +8,67 @@ from stockroom.models.devices import CategoryDev, HistoryDev, StockDev
 
 
 # Consumables
-class StockAdmin(ExportAdmin, admin.ModelAdmin):
-    model = Stockroom
-    list_display = ['stock_model']
+
+@admin.register(Stockroom)
+class StockAdmin(ImportExportModelAdmin):
+    list_display = ['stock_model', 'dateAddToStock', 'dateInstall', 'rack', 'shelf']
     list_filter = ['categories']
-    search_fields = ['stock_model__name']
-    actions = [ExportAdmin.export_to_csv]
+    search_fields = ['stock_model__name', 'stock_model__device__name', 'dateAddToStock', 'dateInstall', 'rack', 'shelf']
 
 
-admin.site.register(Stockroom, StockAdmin)
-
-
-class HistoryAdmin(ExportAdmin, admin.ModelAdmin):
-    model = History
-    list_display = [
-        'stock_model',
-        'stock_model_id',
-        'device',
-        'categories',
-        'quantity',
-        'dateInstall',
-        'user'
-        ]
+@admin.register(History)
+class HistoryAdmin(ImportExportModelAdmin):
+    list_display = ['stock_model', 'stock_model_id', 'device', 'categories', 'quantity', 'dateInstall', 'user']
     list_filter = ['categories']
-    search_fields = [
-        'stock_model',
-        'categories__name',
-        'quantity',
-        'dateInstall',
-        ]
-    actions = [ExportAdmin.export_to_csv]
+    search_fields = ['stock_model', 'categories__name', 'quantity', 'dateInstall', ]
 
 
-admin.site.register(History, HistoryAdmin)
-
-
-class StockCatAdmin(ExportAdmin, admin.ModelAdmin):
-    model = StockCat
-    list_display = [
-        'name',
-        'slug'
-        ]
+@admin.register(StockCat)
+class StockCatAdmin(ImportExportModelAdmin):
+    list_display = ['name', 'slug']
     search_fields = ['name']
-    prepopulated_fields = {"slug": ("name", )}
-    actions = [ExportAdmin.export_to_csv]
-
-
-admin.site.register(StockCat, StockCatAdmin)
+    prepopulated_fields = {"slug": ("name",)}
 
 
 # Accessories
-class StockAccAdmin(ExportAdmin, admin.ModelAdmin):
-    model = StockAcc
-    list_display = ['stock_model']
+@admin.register(StockAcc)
+class StockAccAdmin(ImportExportModelAdmin):
+    list_display = ['stock_model', 'dateAddToStock', 'dateInstall', 'rack', 'shelf']
     list_filter = ['categories']
-    search_fields = ['stock_model__name']
-    actions = [ExportAdmin.export_to_csv]
+    search_fields = ['stock_model__name', 'stock_model__device__name', 'dateAddToStock', 'dateInstall', 'rack', 'shelf']
 
 
-admin.site.register(StockAcc, StockAccAdmin)
-
-
-class HistoryAccAdmin(ExportAdmin, admin.ModelAdmin):
-    model = HistoryAcc
-    list_display = [
-        'stock_model',
-        'stock_model_id',
-        'device',
-        'categories',
-        'quantity',
-        'dateInstall',
-        'user'
-        ]
+@admin.register(HistoryAcc)
+class HistoryAccAdmin(ImportExportModelAdmin):
+    list_display = ['stock_model', 'stock_model_id', 'device', 'categories', 'quantity', 'dateInstall', 'user']
     list_filter = ['categories']
-    search_fields = [
-        'stock_model',
-        'categories__name',
-        'quantity',
-        'dateInstall'
-        ]
-    actions = [ExportAdmin.export_to_csv]
+    search_fields = ['stock_model', 'categories__name', 'quantity', 'dateInstall']
 
 
-admin.site.register(HistoryAcc, HistoryAccAdmin)
-
-
-class CategoryAccAdmin(ExportAdmin, admin.ModelAdmin):
-    model = CategoryAcc
-    list_display = [
-        'name',
-        'slug'
-        ]
+@admin.register(CategoryAcc)
+class CategoryAccAdmin(ImportExportModelAdmin):
+    list_display = ['name', 'slug']
     search_fields = ['name']
-    prepopulated_fields = {"slug": ("name", )}
-    actions = [ExportAdmin.export_to_csv]
-
-
-admin.site.register(CategoryAcc, CategoryAccAdmin)
+    prepopulated_fields = {"slug": ("name",)}
 
 
 # Devices
-class StockDevAdmin(ExportAdmin, admin.ModelAdmin):
-    model = StockDev
-    list_display = ['stock_model', 'dateAddToStock']
+@admin.register(StockDev)
+class StockDevAdmin(ImportExportModelAdmin):
+    list_display = ['stock_model', 'dateAddToStock', 'dateInstall', 'rack', 'shelf']
     list_filter = ['categories']
-    search_fields = [
-        'stock_model__name',
-        'stock_model__invent',
-
-    ]
-    actions = [ExportAdmin.export_to_csv]
+    search_fields = ['stock_model__name', 'stock_model__invent', 'categories__name']
 
 
-admin.site.register(StockDev, StockDevAdmin)
-
-
-class HistoryDevAdmin(ExportAdmin, admin.ModelAdmin):
-    model = HistoryDev
-    list_display = [
-        'stock_model',
-        'stock_model_id',
-        'categories',
-        'quantity',
-        'dateInstall',
-        'user'
-        ]
+@admin.register(HistoryDev)
+class HistoryDevAdmin(ImportExportModelAdmin):
+    list_display = ['stock_model', 'stock_model_id', 'categories', 'quantity', 'dateInstall', 'user']
     list_filter = ['categories']
-    search_fields = [
-        'stock_model',
-        'stock_model_id',
-        'categories__name',
-        'quantity',
-        'dateInstall'
-        ]
-    actions = [ExportAdmin.export_to_csv]
+    search_fields = ['stock_model', 'stock_model_id', 'categories__name', 'quantity', 'dateInstall']
 
 
-admin.site.register(HistoryDev, HistoryDevAdmin)
-
-
-class CategoryDevAdmin(ExportAdmin, admin.ModelAdmin):
-    model = CategoryDev
-    list_display = [
-        'name',
-        'slug'
-        ]
+@admin.register(CategoryDev)
+class CategoryDevAdmin(ImportExportModelAdmin):
+    list_display = ['name', 'slug']
     search_fields = ['name']
-    prepopulated_fields = {"slug": ("name", )}
-    actions = [ExportAdmin.export_to_csv]
-
-
-admin.site.register(CategoryDev, CategoryDevAdmin)
+    prepopulated_fields = {"slug": ("name",)}
