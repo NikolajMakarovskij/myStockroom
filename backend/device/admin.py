75db75ebf9fed59e86fa14f44575169c8fb32838
NikolajMakarovskij <adrameleh666@gmail.com>
2023-11-14 17:04:31 +0500

    add import-export plugin to admin panel in device app

--- backend/device/admin.py
+++ backend/device/admin.py
@@ -1,28 +1,20 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
-from core.utils import ExportAdmin
 from .models import Device, DeviceCat
 
 
-class DeviceAdmin(ExportAdmin, admin.ModelAdmin):
-    model = Device
+@admin.register(Device)
+class DeviceAdmin(ImportExportModelAdmin):
     list_display = ['name', 'description', 'categories', 'manufacturer',
                     'workplace', 'quantity', 'note']
     list_filter = ['manufacturer', 'workplace__room__floor', 'workplace__room__building']
     search_fields = ['name', 'description', 'manufacturer__name', 'serial', 'invent',
                      'workplace__name', 'consumable__name', 'accessories__name', 'quantity', 'note']
-    actions = [ExportAdmin.export_to_csv]
 
 
-admin.site.register(Device, DeviceAdmin)
-
-
-class DeviceCatAdmin(ExportAdmin, admin.ModelAdmin):
-    model = DeviceCat
+@admin.register(DeviceCat)
+class DeviceCatAdmin(ImportExportModelAdmin):
     list_display = ['name', 'slug']
     search_fields = ['name']
     prepopulated_fields = {"slug": ("name",)}
-    actions = [ExportAdmin.export_to_csv]
-
-
-admin.site.register(DeviceCat, DeviceCatAdmin)
