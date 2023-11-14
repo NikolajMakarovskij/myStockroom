c8b2b9cd29d666bb479a842b5bd721663887e54a
NikolajMakarovskij <adrameleh666@gmail.com>
2023-11-14 16:18:12 +0500

    add import-export plugin to admin panel in consumables app

--- backend/consumables/admin.py
+++ backend/consumables/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 
-from core.utils import ExportAdmin
 from .models import Consumables, Categories, AccCat, Accessories
 
 
