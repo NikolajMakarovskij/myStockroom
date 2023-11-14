1ae19f7bf90ef035b5e143f07e3d0acde881f1bf
NikolajMakarovskij <adrameleh666@gmail.com>
2023-11-14 17:29:39 +0500

    add import-export plugin to admin panel in software app

--- backend/software/admin.py
+++ backend/software/admin.py
@@ -1,29 +1,22 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+
 from .models import Software, Os
-from core.utils import ExportAdmin
 
 
-class SoftwareAdmin(ExportAdmin, admin.ModelAdmin):
-    model = Software
+@admin.register(Software)
+class SoftwareAdmin(ImportExportModelAdmin):
     list_display = ['name', 'manufacturer', 'version', 'bitDepth', 'licenseKeyText', 'licenseKeyImg',
                     'licenseKeyFile', ]
     list_filter = ['manufacturer', 'bitDepth', ]
     search_fields = ['name', 'manufacturer', 'version', 'bitDepth', 'licenseKeyText', 'licenseKeyImg',
                      'licenseKeyFile', ]
-    actions = [ExportAdmin.export_to_csv]
 
 
-admin.site.register(Software, SoftwareAdmin)
-
-
-class OsAdmin(ExportAdmin, admin.ModelAdmin):
-    model = Os
+@admin.register(Os)
+class OsAdmin(ImportExportModelAdmin):
     list_display = ['name', 'manufacturer', 'version', 'bitDepth', 'licenseKeyText', 'licenseKeyImg',
                     'licenseKeyFile', ]
     list_filter = ['manufacturer', 'bitDepth', ]
     search_fields = ['name', 'manufacturer', 'version', 'bitDepth', 'licenseKeyText', 'licenseKeyImg',
                      'licenseKeyFile', ]
-    actions = [ExportAdmin.export_to_csv]
-
-
-admin.site.register(Os, OsAdmin)
