769c22f1a20bd911486692acb6a69bd6578c2c5e
NikolajMakarovskij <adrameleh666@gmail.com>
2023-11-16 10:54:06 +0500

    update resource in device and stock apps

--- backend/stockroom/resources.py
+++ backend/stockroom/resources.py
@@ -1,28 +1,92 @@
 from import_export import fields, resources
+from import_export.widgets import ForeignKeyWidget, ManyToManyWidget
 
-from stockroom.models.devices import StockDev
+from device.models import Device
+from stockroom.models.devices import StockDev, CategoryDev
 
 
 class StockDevResource(resources.ModelResource):
+    name = fields.Field(
+        column_name='Название',
+        attribute='stock_model',
+        widget=ForeignKeyWidget(Device, field='name')
+    )
+    hostname = fields.Field(
+        column_name='Имя хоста',
+        attribute='stock_model',
+        widget=ForeignKeyWidget(Device, field='hostname')
+    )
+    ip_address = fields.Field(
+        column_name='IP адрес',
+        attribute='stock_model',
+        widget=ForeignKeyWidget(Device, field='ip_address')
+    )
+    categories = fields.Field(
+        column_name='Категория',
+        attribute='categories',
+        widget=ForeignKeyWidget(CategoryDev, field='name')
+    )
+    manufacturer = fields.Field(
+        column_name='Производитель',
+        attribute='stock_model',
+        widget=ForeignKeyWidget(Device, field='manufacturer__name')
+    )
+    serial = fields.Field(
+        column_name='Серийный номер',
+        attribute='stock_model',
+        widget=ForeignKeyWidget(Device, field='serial')
+    )
+    serialImg = fields.Field(
+        column_name='Фото серийного номера',
+        attribute='stock_model',
+        widget=ForeignKeyWidget(Device, field='serialImg')
+    )
+    invent = fields.Field(
+        column_name='Инвентарный номер',
+        attribute='stock_model',
+        widget=ForeignKeyWidget(Device, field='invent')
+    )
+    inventImg = fields.Field(
+        column_name='Фото инвентарного номера',
+        attribute='stock_model',
+        widget=ForeignKeyWidget(Device, field='inventImg')
+    )
+    description = fields.Field(
+        column_name='Описание',
+        attribute='stock_model',
+        widget=ForeignKeyWidget(Device, field='description')
+    )
+    workplace = fields.Field(
+        column_name='Рабочее место',
+        attribute='stock_model',
+        widget=ForeignKeyWidget(Device, field='workplace__name')
+    )
+    quantity = fields.Field(
+        column_name='Количество',
+        attribute='stock_model',
+        widget=ForeignKeyWidget(Device, field='quantity')
+    )
+    note = fields.Field(
+        column_name='Примечание',
+        attribute='stock_model',
+        widget=ForeignKeyWidget(Device, field='note')
+    )
+    dateAddToStock = fields.Field(
+        column_name='Дата постановки',
+        attribute='dateAddToStock'
+    )
+    dateInstall = fields.Field(
+        column_name="Дата установки",
+        attribute='dateInstall'
+        )
+    rack = fields.Field(
+        column_name="Стеллаж",
+        attribute='rack'
+        )
+    shelf = fields.Field(
+        column_name="Полка",
+        attribute='shelf'
+        )
 
     class Meta:
         model = StockDev
-        fields = [
-            'stock_model__name',
-            'stock_model__categories__name',
-            'stock_model__serial',
-            'stock_model__serialImg',
-            'stock_model__invent',
-            'stock_model__inventImg',
-            'stock_model__hostname',
-            'stock_model__ip_address',
-            'stock_model__login',
-            'stock_model__pwd',
-            'stock_model__workplace__name',
-            'stock_model__accessories__name',
-            'stock_model__consumable__name',
-            'stock_model__manufacturer__name',
-            'stock_model__description',
-            'stock_model__note',
-            'dateAddToStock'
-        ]
