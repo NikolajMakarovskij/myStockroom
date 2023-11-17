276c001a6e1abd479daf4251bcee7a40f7b97a2c
NikolajMakarovskij <adrameleh666@gmail.com>
2023-11-17 08:48:45 +0500

    refactor stock resources

--- backend/stockroom/resources.py
+++ backend/stockroom/resources.py
@@ -1,6 +1,7 @@
 from import_export import fields, resources
 from import_export.widgets import ForeignKeyWidget, ManyToManyWidget
 
+from dataclasses import dataclass
 from device.models import Device
 from consumables.models import Consumables, Accessories
 from stockroom.models.devices import StockDev, CategoryDev
@@ -10,235 +11,121 @@ from stockroom.models.accessories import StockAcc, CategoryAcc, HistoryAcc
 from datetime import datetime
 
 
-class StockDevResource(resources.ModelResource):
+@dataclass
+class BaseStockResource(resources.ModelResource):
+    """Class with stock base methods"""
+    stock_model: dict = None
+    stock_category: dict = None
+
     name = fields.Field(
         column_name='Название',
         attribute='stock_model',
-        widget=ForeignKeyWidget(Device, field='name')
+        widget=ForeignKeyWidget(stock_model, field='name')
     )
+    categories = fields.Field(
+        column_name='Категория',
+        attribute='categories',
+        widget=ForeignKeyWidget(stock_category, field='name')
+    )
+    manufacturer = fields.Field(
+        column_name='Производитель',
+        attribute='stock_model',
+        widget=ForeignKeyWidget(stock_model, field='manufacturer__name')
+    )
+    serial = fields.Field(
+        column_name='Серийный номер',
+        attribute='stock_model',
+        widget=ForeignKeyWidget(stock_model, field='serial')
+    )
+    serialImg = fields.Field(
+        column_name='Фото серийного номера',
+        attribute='stock_model',
+        widget=ForeignKeyWidget(stock_model, field='serialImg')
+    )
+    invent = fields.Field(
+        column_name='Инвентарный номер',
+        attribute='stock_model',
+        widget=ForeignKeyWidget(stock_model, field='invent')
+    )
+    inventImg = fields.Field(
+        column_name='Фото инвентарного номера',
+        attribute='stock_model',
+        widget=ForeignKeyWidget(stock_model, field='inventImg')
+    )
+    description = fields.Field(
+        column_name='Описание',
+        attribute='stock_model',
+        widget=ForeignKeyWidget(stock_model, field='description')
+    )
+    quantity = fields.Field(
+        column_name='Количество',
+        attribute='stock_model',
+        widget=ForeignKeyWidget(stock_model, field='quantity')
+    )
+    note = fields.Field(
+        column_name='Примечание',
+        attribute='stock_model',
+        widget=ForeignKeyWidget(stock_model, field='note')
+    )
+    dateAddToStock = fields.Field(
+        column_name='Дата поступления на склад',
+        attribute='dateAddToStock'
+    )
+    dateInstall = fields.Field(
+        column_name="Дата установки",
+        attribute='dateInstall'
+    )
+    rack = fields.Field(
+        column_name="Стеллаж",
+        attribute='rack'
+    )
+    shelf = fields.Field(
+        column_name="Полка",
+        attribute='shelf'
+    )
+
+
+class StockDevResource(BaseStockResource):
+    stock_model = Device
+    stock_category = CategoryDev
+
     hostname = fields.Field(
         column_name='Имя хоста',
         attribute='stock_model',
-        widget=ForeignKeyWidget(Device, field='hostname')
+        widget=ForeignKeyWidget(stock_model, field='hostname')
     )
     ip_address = fields.Field(
         column_name='IP адрес',
         attribute='stock_model',
-        widget=ForeignKeyWidget(Device, field='ip_address')
-    )
-    categories = fields.Field(
-        column_name='Категория',
-        attribute='categories',
-        widget=ForeignKeyWidget(CategoryDev, field='name')
-    )
-    manufacturer = fields.Field(
-        column_name='Производитель',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Device, field='manufacturer__name')
-    )
-    serial = fields.Field(
-        column_name='Серийный номер',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Device, field='serial')
-    )
-    serialImg = fields.Field(
-        column_name='Фото серийного номера',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Device, field='serialImg')
-    )
-    invent = fields.Field(
-        column_name='Инвентарный номер',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Device, field='invent')
-    )
-    inventImg = fields.Field(
-        column_name='Фото инвентарного номера',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Device, field='inventImg')
-    )
-    description = fields.Field(
-        column_name='Описание',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Device, field='description')
+        widget=ForeignKeyWidget(stock_model, field='ip_address')
     )
     workplace = fields.Field(
         column_name='Рабочее место',
         attribute='stock_model',
-        widget=ForeignKeyWidget(Device, field='workplace__name')
+        widget=ForeignKeyWidget(stock_model, field='workplace__name')
     )
-    quantity = fields.Field(
-        column_name='Количество',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Device, field='quantity')
-    )
-    note = fields.Field(
-        column_name='Примечание',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Device, field='note')
-    )
-    dateAddToStock = fields.Field(
-        column_name='Дата постановки',
-        attribute='dateAddToStock'
-    )
-    dateInstall = fields.Field(
-        column_name="Дата установки",
-        attribute='dateInstall'
-        )
-    rack = fields.Field(
-        column_name="Стеллаж",
-        attribute='rack'
-        )
-    shelf = fields.Field(
-        column_name="Полка",
-        attribute='shelf'
-        )
 
     class Meta:
         model = StockDev
+        exclude = ['stock_model']
 
 
-class StockConResource(resources.ModelResource):
-    name = fields.Field(
-        column_name='Название',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Consumables, field='name')
-    )
-    categories = fields.Field(
-        column_name='Категория',
-        attribute='categories',
-        widget=ForeignKeyWidget(StockCat, field='name')
-    )
-    manufacturer = fields.Field(
-        column_name='Производитель',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Consumables, field='manufacturer__name')
-    )
-    serial = fields.Field(
-        column_name='Серийный номер',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Consumables, field='serial')
-    )
-    serialImg = fields.Field(
-        column_name='Фото серийного номера',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Consumables, field='serialImg')
-    )
-    invent = fields.Field(
-        column_name='Инвентарный номер',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Consumables, field='invent')
-    )
-    inventImg = fields.Field(
-        column_name='Фото инвентарного номера',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Consumables, field='inventImg')
-    )
-    description = fields.Field(
-        column_name='Описание',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Consumables, field='description')
-    )
-    quantity = fields.Field(
-        column_name='Количество',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Consumables, field='quantity')
-    )
-    note = fields.Field(
-        column_name='Примечание',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Consumables, field='note')
-    )
-    dateAddToStock = fields.Field(
-        column_name='Дата поступления на склад',
-        attribute='dateAddToStock'
-    )
-    dateInstall = fields.Field(
-        column_name="Дата установки",
-        attribute='dateInstall'
-        )
-    rack = fields.Field(
-        column_name="Стеллаж",
-        attribute='rack'
-        )
-    shelf = fields.Field(
-        column_name="Полка",
-        attribute='shelf'
-        )
+class StockConResource(BaseStockResource):
+    stock_model = Consumables
+    stock_category = StockCat
 
     class Meta:
         model = Stockroom
+        exclude = ['stock_model']
 
 
-class StockAccResource(resources.ModelResource):
-    name = fields.Field(
-        column_name='Название',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Accessories, field='name')
-    )
-    categories = fields.Field(
-        column_name='Категория',
-        attribute='categories',
-        widget=ForeignKeyWidget(CategoryAcc, field='name')
-    )
-    manufacturer = fields.Field(
-        column_name='Производитель',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Accessories, field='manufacturer__name')
-    )
-    serial = fields.Field(
-        column_name='Серийный номер',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Accessories, field='serial')
-    )
-    serialImg = fields.Field(
-        column_name='Фото серийного номера',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Accessories, field='serialImg')
-    )
-    invent = fields.Field(
-        column_name='Инвентарный номер',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Accessories, field='invent')
-    )
-    inventImg = fields.Field(
-        column_name='Фото инвентарного номера',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Accessories, field='inventImg')
-    )
-    description = fields.Field(
-        column_name='Описание',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Accessories, field='description')
-    )
-    quantity = fields.Field(
-        column_name='Количество',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Accessories, field='quantity')
-    )
-    note = fields.Field(
-        column_name='Примечание',
-        attribute='stock_model',
-        widget=ForeignKeyWidget(Accessories, field='note')
-    )
-    dateAddToStock = fields.Field(
-        column_name='Дата поступления на склад',
-        attribute='dateAddToStock'
-    )
-    dateInstall = fields.Field(
-        column_name="Дата установки",
-        attribute='dateInstall'
-        )
-    rack = fields.Field(
-        column_name="Стеллаж",
-        attribute='rack'
-        )
-    shelf = fields.Field(
-        column_name="Полка",
-        attribute='shelf'
-        )
+class StockAccResource(BaseStockResource):
+    stock_model = Accessories
+    stock_category = CategoryAcc
 
     class Meta:
         model = StockAcc
+        exclude = ['stock_model']
 
 
 class ConsumptionResource(resources.ModelResource):
@@ -265,13 +152,12 @@ class ConsumptionResource(resources.ModelResource):
         attribute='quantity',
     )
 
-
     def get_queryset(self):
         return self._meta.model.objects.filter(status='Расход').order_by('stock_model').distinct('stock_model')
 
     class Meta:
         model = History
-        exclude = ['id', 'stock_model_id', 'device', 'deviceId', 'categories', 'dateInstall', 'user', 'status',	'note']
+        exclude = ['id', 'stock_model_id', 'device', 'deviceId', 'categories', 'dateInstall', 'user', 'status', 'note']
 
     def dehydrate_stock_model(self, history):
         name = getattr(history, "stock_model")
@@ -299,7 +185,7 @@ class ConsumptionResource(resources.ModelResource):
             else:
                 devices = consumable.device.all().order_by('name').distinct('name')
                 for device in devices:
-                     device_list.append(device.name)
+                    device_list.append(device.name)
                 devices = '|'.join(device_list)
         return devices
 
@@ -336,8 +222,8 @@ class ConsumptionResource(resources.ModelResource):
         unit_history_last_year = history.filter(
             stock_model_id=id,
             status='Расход',
-            dateInstall__gte=f"{int(cur_year.strftime('%Y'))-1}-01-01",
-            dateInstall__lte=f"{int(cur_year.strftime('%Y'))-1}-12-31"
+            dateInstall__gte=f"{int(cur_year.strftime('%Y')) - 1}-01-01",
+            dateInstall__lte=f"{int(cur_year.strftime('%Y')) - 1}-12-31"
         )
         for unit in unit_history_last_year:
             quantity_last_year += unit.quantity
@@ -357,5 +243,3 @@ class ConsumptionResource(resources.ModelResource):
         for unit in unit_history_current_year:
             quantity_current_year += unit.quantity
         return quantity_current_year
-
-
