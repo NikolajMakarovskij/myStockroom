7be3e91b0f62146688f3a6262002e924c0c8925c
NikolajMakarovskij <adrameleh666@gmail.com>
2023-11-17 09:47:12 +0500

    refactor resource

--- backend/consumables/resources.py
+++ backend/consumables/resources.py
@@ -3,9 +3,13 @@ from import_export.widgets import ForeignKeyWidget, ManyToManyWidget
 
 from consumables.models import Consumables, Accessories, Categories, AccCat
 from counterparty.models import Manufacturer
+from dataclasses import dataclass
 
+@dataclass
+class BaseResource(resources.ModelResource):
+    category_model: dict = None
+    manufacturer_model: dict = None
 
-class ConsumableResource(resources.ModelResource):
     name = fields.Field(
         column_name='Название',
         attribute='name'
@@ -13,12 +17,12 @@ class ConsumableResource(resources.ModelResource):
     categories = fields.Field(
         column_name='Категория',
         attribute='categories',
-        widget=ForeignKeyWidget(Categories, field='name')
+        widget=ForeignKeyWidget(category_model, field='name')
     )
     manufacturer = fields.Field(
         column_name='Производитель',
         attribute='manufacturer',
-        widget=ForeignKeyWidget(Manufacturer, field='name')
+        widget=ForeignKeyWidget(manufacturer_model, field='name')
     )
     serial = fields.Field(
         column_name='Серийный номер',
@@ -48,54 +52,21 @@ class ConsumableResource(resources.ModelResource):
         column_name='Примечание',
         attribute='note'
     )
+
+
+class ConsumableResource(BaseResource):
+    category_model = Categories
+    manufacturer_model = Manufacturer
 
     class Meta:
         model = Consumables
+        exclude = ['id']
 
 
-class AccessoriesResource(resources.ModelResource):
-    name = fields.Field(
-        column_name='Название',
-        attribute='name'
-    )
-    categories = fields.Field(
-        column_name='Категория',
-        attribute='categories',
-        widget=ForeignKeyWidget(AccCat, field='name')
-    )
-    manufacturer = fields.Field(
-        column_name='Производитель',
-        attribute='manufacturer',
-        widget=ForeignKeyWidget(Manufacturer, field='name')
-    )
-    serial = fields.Field(
-        column_name='Серийный номер',
-        attribute='serial'
-    )
-    serialImg = fields.Field(
-        column_name='Фото серийного номера',
-        attribute='serialImg'
-    )
-    invent = fields.Field(
-        column_name='Инвентарный номер',
-        attribute='invent'
-    )
-    inventImg = fields.Field(
-        column_name='Фото инвентарного номера',
-        attribute='inventImg'
-    )
-    description = fields.Field(
-        column_name='Описание',
-        attribute='description'
-    )
-    quantity = fields.Field(
-        column_name='Количество',
-        attribute='quantity'
-    )
-    note = fields.Field(
-        column_name='Примечание',
-        attribute='note'
-    )
+class AccessoriesResource(BaseResource):
+    category_model = AccCat
+    manufacturer_model = Manufacturer
 
     class Meta:
         model = Accessories
+        exclude = ['id']
--- backend/device/resources.py
+++ backend/device/resources.py
@@ -76,3 +76,4 @@ class DeviceResource(resources.ModelResource):
 
     class Meta:
         model = Device
+        exclude = ['id', 'login', 'pwd']
