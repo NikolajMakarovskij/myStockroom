7623a71741876fbd3b037026127705d274e54b43
makarovskiynv <adrameleh666@gmail.com>
2023-12-08 12:26:42 +0500

    complete production docker scripts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-export const API_URL = "http://localhost:8010/workplace/api/v1/room/"
+export const API_URL = "http://web:8010/workplace/api/v1/room/"
 export const API_STATIC_MEDIA = "http://localhost/static"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
