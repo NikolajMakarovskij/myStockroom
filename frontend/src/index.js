ee4eb04ae97436b0d43bb15eb19dbbd2db95095e
makarovskiynv <adrameleh666@gmail.com>
2023-12-14 17:15:44 +0500

    change api_url

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,8 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
-export const API_URL = "http://172.18.0.6:8010/"
-export const API_STATIC_MEDIA = "http://172.18.0.4/static/"
+export const API_URL = "http://172.18.0.7:8010/"
+export const API_STATIC_MEDIA = "http://172.18.0.2/static/"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
