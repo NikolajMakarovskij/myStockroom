26caf02f3c06644b1f88795a82e2cdcf1f426080
NikolajMakarovskij <adrameleh666@gmail.com>
2023-12-15 21:18:00 +0500

    create form fields

---
+++ frontend/src/components/appWorkplace/Rooms/CreateRoom.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import {Container} from "reactstrap";
+
+const CreateRoom = () => {
+
+    return(
+        <Container>
+
+        </Container>
+    )
+
+
+}
+
+export default CreateRoom
---
+++ frontend/src/components/appWorkplace/Rooms/HomeRoom.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import {Container} from "reactstrap";
+import CreateRoom from "./CreateRoom";
+
+
+
+const HomeRoom = () => {
+
+    return(
+        <Container>
+            <CreateRoom/>
+        </Container>
+    )
+
+
+}
+
+export default HomeRoom
