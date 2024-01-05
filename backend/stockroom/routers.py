4ad1e78b3a386847e61ca858c5ddadd5f80dded2
NikolajMakarovskij <adrameleh666@gmail.com>
2024-01-05 19:20:32 +0500

    add tabs in stock devices list

--- backend/stockroom/routers.py
+++ backend/stockroom/routers.py
@@ -1,8 +1,9 @@
 from .views.consumables import StockRestView
-from .views.devices import StockDevListRestView
+from .views.devices import StockDevListRestView, StockDevCatListRestView
 from rest_framework import routers
 
 
 router = routers.SimpleRouter()
 router.register(r'stock', StockRestView)
 router.register(r'stock_dev_list', StockDevListRestView)
+router.register(r'stock_dev_cat', StockDevCatListRestView)
--- frontend/src/components/appStock/Devices/ListStockDevices.jsx
+++ frontend/src/components/appStock/Devices/ListStockDevices.jsx
@@ -1,36 +1,83 @@
-import {React, useEffect, useMemo, useState, useCallback} from 'react'
+import {React, useMemo, useState, useCallback, useEffect,} from 'react'
+import PropTypes from 'prop-types';
 import {
     MaterialReactTable,
 } from 'material-react-table';
 //import {createTheme} from "@mui/material/styles";
 import AxiosInstanse from "../../Axios";
 import {Link} from "react-router-dom";
-import {IconButton, MenuItem,} from '@mui/material';
+import {IconButton, MenuItem, Box, Tabs, Tab} from '@mui/material';
 import {Add as AddIcon, Edit as EditIcon, Delete as DeleteIcon,} from '@mui/icons-material';
 import {MRT_Localization_RU} from 'material-react-table/locales/ru';
-import LinearIndeterminate from "../../appHome/ProgressBar";
+import LinearIndeterminate from "../../appHome/ProgressBar.jsx";
 
 /*const darkTheme = createTheme({
   palette: {
     mode: 'dark',
   },
 });*/
+function samePageLinkNavigation(event) {
+  if (
+    event.defaultPrevented ||
+    event.button !== 0 || // ignore everything but left-click
+    event.metaKey ||
+    event.ctrlKey ||
+    event.altKey ||
+    event.shiftKey
+  ) {
+    return false;
+  }
+  return true;
+}
 
-const ListStockDevices = () => {
+function LinkTab(props) {
+  return (
+    <Tab
+      component="a"
+      onClick={(event) => {
+        if (samePageLinkNavigation(event)) {
+          event.preventDefault();
+        }
+      }}
+      aria-current={props.selected && 'page'}
+      {...props}
+    />
+  );
+}
+
+LinkTab.propTypes = {
+  selected: PropTypes.bool,
+};
+export default function ListStockDevices() {
+    const [value, setValue] = useState(0);
     const [device, setDevices] = useState()
+    const [category, setCategory] = useState('')
     const [loading, setLoading] = useState(true)
 
     const GetData = useCallback(async () => {
-         await AxiosInstanse.get(`stockroom/api/v1/stock_dev_list/`, {timeout: 1000*30}).then((res) => {
+        await AxiosInstanse.get(`stockroom/api/v1/stock_dev_list/`, {timeout: 1000*30}).then((res) => {
             setDevices(res.data)
+        })
+        await AxiosInstanse.get(`stockroom/api/v1/stock_dev_cat/`).then((res) => {
+            setCategory(res.data)
             setLoading(false)
         })
+
     })
 
     useEffect(() =>{
         GetData();
     },[])
 
+  const handleChange = (event, newValue) => {
+    if (
+      event.type !== 'click' ||
+      (event.type === 'click' && samePageLinkNavigation(event))
+    ) {
+      setValue(newValue);
+      setCategory(newValue.slug)
+    }
+  };
     const columns = useMemo(() => [
         {
             accessorKey: 'stock_model.name',
@@ -88,43 +135,53 @@ const ListStockDevices = () => {
     );
 
     return(
-        <div>
+        <>
             {loading ? <LinearIndeterminate/> :
-                <MaterialReactTable
-                    columns={columns}
-                    data={device}
-                    localization={MRT_Localization_RU}
-                    enableColumnResizing={true}
-                    positionPagination='both'
-                    enableRowNumbers='true'
-                    enableRowVirtualization='true'
-                    enableRowActions
-                    renderRowActionMenuItems={({
-                        row,
-                        menuActions = [
-                            //{'name': 'Добавить', 'path': `create`, 'icon': <AddIcon/>, 'color': 'success',},
-                            //{'name': 'Редактировать', 'path': `edit/${row.original.id}`, 'icon': <EditIcon/>, 'color': 'primary',},
-                            //{'name': 'Удалить', 'path': `remove/${row.original.id}`, 'icon': <DeleteIcon/>, 'color': 'error',},
-                        ], }) => [
-
-                        menuActions.map(
-                            (item, index) => (
-                            <MenuItem key={index} component={Link} to={item.path}>
-                                <IconButton color={item.color} >
-                                    {item.icon}
-                                </IconButton>
-                                {item.name}
-                            </MenuItem>
-                        ))
-                    ]}
-                />
+                (
+                    <Box>
+                        <Tabs
+                            value={value}
+                            onChange={handleChange}
+                            aria-label="nav tabs example"
+                            role="navigation"
+                        >
+                            <LinkTab label="Все" href="/stock/device/list" />
+                            {category.map((cat, index) => (
+                                <LinkTab label={cat.name} href='/stock/device/list/{cat.slug}' key={index} />
+                            ))}
+                        </Tabs>
+                        <MaterialReactTable
+                            columns={columns}
+                            data={device}
+                            localization={MRT_Localization_RU}
+                            enableColumnResizing={true}
+                            positionPagination='both'
+                            enableRowNumbers='true'
+                            enableRowVirtualization='true'
+                            enableRowActions
+                            renderRowActionMenuItems={({row,
+                                menuActions = [
+                                //{'name': 'Добавить', 'path': `create`, 'icon': <AddIcon/>, 'color': 'success',},
+                                //{'name': 'Редактировать', 'path': `edit/${row.original.id}`, 'icon': <EditIcon/>, 'color': 'primary',},
+                                //{'name': 'Удалить', 'path': `remove/${row.original.id}`, 'icon': <DeleteIcon/>, 'color': 'error',},
+                            ], }) => [
+                                menuActions.map(
+                                    (item, index) => (
+                                    <MenuItem key={index} component={Link} to={item.path}>
+                                        <IconButton color={item.color} >
+                                            {item.icon}
+                                        </IconButton>
+                                        {item.name}
+                                    </MenuItem>
+                                ))
+                            ]}
+                        />
+                    </Box>
+                )
             }
-
-        </div>
+        </>
     )
 
 
 
 };
-
-export default ListStockDevices
