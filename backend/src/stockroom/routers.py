58199d883e0e8d740f24c8d002c90cc662959ea1
NikolajMakarovskij <adrameleh666@gmail.com>
2024-01-05 13:12:47 +0500

    add api stock devices list

--- backend/src/stockroom/routers.py
+++ backend/src/stockroom/routers.py
@@ -1,7 +1,9 @@
+from .views.consumables import StockRestView
+from .views.devices import StockDevListRestView
 from rest_framework import routers
 
 from .views.consumables import StockRestView
 
 router = routers.SimpleRouter()
-router.register(r"stock", StockRestView)
-# router.register(r'device_cat', DeviceCatRestView)
+router.register(r'stock', StockRestView)
+router.register(r'stock_dev_list', StockDevListRestView)
