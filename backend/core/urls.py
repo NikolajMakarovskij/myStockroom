76a9f9eb702aa5e017ab3b5fa75907b716a56df5
NikolajMakarovskij <adrameleh666@gmail.com>
2024-01-19 17:09:46 +0500

    add auth app

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, UserView, UserRegister, UserLogin, UserLogout
 
 urlpatterns = [
     # главная
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
+    path('api/register/', UserRegister.as_view(), name='register'),
+    path('api/login/', UserLogin.as_view(), name='login'),
+    path('api/logout/', UserLogout.as_view(), name='logout'),
+    path('api/user/', UserView.as_view(), name='user'),
 
 ]
