a5bfd167eaf8a68b7907f71095512d5ab9d54cb6
NikolajMakarovskij <adrameleh666@gmail.com>
2024-02-15 15:23:07 +0500

    add Modal

---
+++ frontend/src/components/Modal/Modal.jsx
@@ -0,0 +1,24 @@
+import { createPortal } from 'react-dom'
+import { useRef, useEffect } from 'react'
+import './Modal.css'
+
+export default function Modal({children, open}) {
+    const dialog = useRef()
+
+    useEffect(() => {
+        if (open) {
+            dialog.current.showModal()
+        } else {
+            dialog.current.close()
+        }
+    }, [open])
+
+
+
+    return createPortal(
+        <dialog ref={dialog}>
+            {children}
+        </dialog>,
+        document.getElementById('modal')
+    )
+}
