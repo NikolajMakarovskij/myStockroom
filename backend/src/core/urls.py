c20f398ee5e16f976a2ee32c739332cee7be7bd3
NikolajMakarovskij <adrameleh666@gmail.com>
2024-02-27 09:11:39 +0500

    delete old auth API

--- backend/src/core/urls.py
+++ backend/src/core/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
-from .views import IndexView, UserView, UserRegister, UserLogin, UserLogout
+from .views import IndexView
 
 urlpatterns = [
     # главная
     path('', IndexView.as_view(), name='index'),
-    path('api/register/', UserRegister.as_view(), name='register'),
-    path('api/login/', UserLogin.as_view(), name='login'),
-    path('api/logout/', UserLogout.as_view(), name='logout'),
-    path('api/user/', UserView.as_view(), name='user'),
 
 ]
