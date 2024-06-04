5b57c15746cc8dd96e78bd7e6b19c3a042d4a6a5
NikolajMakarovskij <adrameleh666@gmail.com>
2024-06-04 13:09:59 +0500

    update celery

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -3,12 +3,12 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-app = Celery('backend')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app = Celery('backend') # type: ignore
+app.config_from_object('django.conf:settings', namespace='CELERY') # type: ignore
 app.autodiscover_tasks()
 app.conf.timezone = 'Asia/Yekaterinburg'
 
 
-@app.task(bind=True)
+@app.task(bind=True) 
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
