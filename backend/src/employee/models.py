852a003640e72b6e960aa8387bcb45170eb5ce4a
NikolajMakarovskij <adrameleh666@gmail.com>
2024-11-08 22:01:13 +0500

    add employee app docs

--- backend/src/employee/models.py
+++ backend/src/employee/models.py
@@ -8,8 +8,11 @@ from workplace.models import Workplace
 
 
 class Employee(ModelMixin, models.Model):
-    """
-    Модель сотрудника. Связи один ко многим с моделями workstation, signature
+    """_Employee_:
+    Employee model
+
+    Returns:
+        Employee (Employee): _description_
     """
 
     id: models.UUIDField = models.UUIDField(
@@ -48,12 +51,27 @@ class Employee(ModelMixin, models.Model):
     )
 
     def __str__(self):
+        """_Employee __str__ _: _returns name of model_
+
+        Returns:
+            Employee__name (str): _returns name_
+        """
         return self.name
 
     def get_absolute_url(self):
+        """_Employee url_
+
+        Returns:
+            Employee__id (str): _returns url by id_
+
+        Other parameters:
+            args (str): self.id
+        """
         return reverse("employee:employee-detail", args=[str(self.id)])
 
     class Meta:
+        """_Employee Meta_: _model settings_
+        """
         verbose_name = "Сотрудник"
         verbose_name_plural = "Сотрудники"
         ordering = [
@@ -64,18 +82,39 @@ class Employee(ModelMixin, models.Model):
 
 
 class Departament(ModelMixin, models.Model):
+    """_Departament_:
+    Departament model
+
+    Returns:
+        Departament (Departament): _description_
+    """
     id: models.UUIDField = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="ID")
     name: models.CharField = models.CharField(
         max_length=50, help_text="Введите название отдела", verbose_name="Отдел"
     )
 
     def __str__(self):
+        """_Departament __str__ _: _returns name of model_
+
+        Returns:
+            Departament__name (str): _returns name_
+        """
         return self.name
 
     def get_absolute_url(self):
+        """_Departament url_
+
+        Returns:
+            Departament__id (str): _returns url by id_
+
+        Other parameters:
+            args (str): self.id
+        """
         return reverse("employee:departament-detail", args=[str(self.id)])
 
     class Meta:
+        """_Departament Meta_: _model settings_
+        """
         verbose_name = "Отдел"
         verbose_name_plural = "Отделы"
         ordering = [
@@ -84,6 +123,12 @@ class Departament(ModelMixin, models.Model):
 
 
 class Post(ModelMixin, models.Model):
+    """_Post_:
+    Post model
+
+    Returns:
+        Post (Post): _description_
+    """
     id: models.UUIDField = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="ID")
     name: models.CharField = models.CharField(
         max_length=50, help_text="Введите должность", verbose_name="Должность"
@@ -98,12 +143,27 @@ class Post(ModelMixin, models.Model):
     )
 
     def __str__(self):
+        """_Post __str__ _: _returns name of model_
+
+        Returns:
+            Post__name (str): _returns name_
+        """
         return self.name
 
     def get_absolute_url(self):
+        """_Post url_
+
+        Returns:
+            Post__id (str): _returns url by id_
+
+        Other parameters:
+            args (str): self.id
+        """
         return reverse("employee:post-detail", args=[str(self.id)])
 
     class Meta:
+        """_Post Meta_: _model settings_
+        """
         verbose_name = "Должность"
         verbose_name_plural = "Должности"
         ordering = [
