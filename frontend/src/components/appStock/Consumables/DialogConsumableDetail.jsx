52533544f19de2bbd28db600ba3402a881024f66
NikolajMakarovskij <adrameleh666@gmail.com>
2025-04-18 15:38:47 +0500

    added detail dialogs in history

---
+++ frontend/src/components/appStock/Consumables/DialogConsumableDetail.jsx
@@ -0,0 +1,119 @@
+import { React, useEffect, useState, useCallback, Fragment } from 'react'
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  IconButton,
+  Table,
+  TableHead,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Toolbar,
+  Typography,
+} from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
+import AxiosInstanse from '../../Axios.jsx'
+import LinearIndeterminate from '../../appHome/ProgressBar.jsx'
+import PrintError from '../../Errors/Error.jsx'
+import PropTypes from 'prop-types'
+
+export default function DialogConsumableDetail(props) {
+  const { consumable, id } = props
+  const [open, setOpen] = useState(false)
+  const [detail, setDetail] = useState()
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+
+  const GetData = useCallback(async () => {
+    try {
+      await AxiosInstanse.get(`consumables/consumable_list/${id}/`).then((res) => {
+        setDetail(res.data)
+      })
+    } catch (error) {
+      setError(error.message)
+    } finally {
+      setLoading(false)
+    }
+  })
+
+  useEffect(() => {
+    GetData()
+  })
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  return (
+    <Fragment>
+      <Button variant='outlined' onClick={handleClickOpen} style={{ textTransform: 'none' }}>
+        {consumable}
+      </Button>
+      <Dialog onClose={handleClose} fullWidth='true' maxWidth='sm' open={open}>
+        <DialogTitle sx={{ m: 0, p: 2 }} id='consumables'>
+          {loading ? <LinearIndeterminate /> : error ? <PrintError error={error} /> : `${detail.name}`}
+        </DialogTitle>
+        <IconButton
+          aria-label='close'
+          onClick={handleClose}
+          sx={(theme) => ({
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: theme.palette.grey[500],
+          })}
+        >
+          <CloseIcon />
+        </IconButton>
+        <DialogContent dividers>
+          {loading ? (
+            <LinearIndeterminate />
+          ) : error ? (
+            <PrintError error={error} />
+          ) : detail.consumable.length == 0 ? (
+            <Typography>На балансе нет</Typography>
+          ) : (
+            <TableContainer>
+              <Toolbar>
+                <Typography>Разность: {detail.difference}</Typography>
+              </Toolbar>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Название:</TableCell>
+                    <TableCell>Счет:</TableCell>
+                    <TableCell>Код:</TableCell>
+                    <TableCell>Остаток:</TableCell>
+                    <TableCell>Стоимость:</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {detail.consumable.map((item, index) => (
+                    <TableRow key={index}>
+                      <TableCell>{item.name}</TableCell>
+                      <TableCell>{item.account}</TableCell>
+                      <TableCell>{item.code}</TableCell>
+                      <TableCell>{item.quantity}</TableCell>
+                      <TableCell>{item.cost}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          )}
+        </DialogContent>
+      </Dialog>
+    </Fragment>
+  )
+}
+
+DialogConsumableDetail.propTypes = {
+  consumable: PropTypes.string,
+  id: PropTypes.string,
+}
