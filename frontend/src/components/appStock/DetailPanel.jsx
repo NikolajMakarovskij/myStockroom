6d3c1a1b8f8616492f40abc72d274be9e090dbfd
NikolajMakarovskij <adrameleh666@gmail.com>
2025-04-22 16:50:28 +0500

    fix errors

--- frontend/src/components/appStock/DetailPanel.jsx
+++ frontend/src/components/appStock/DetailPanel.jsx
@@ -1,9 +1,11 @@
-import PrintError from '../Errors/Error'
+import { React } from 'react'
 import { useState, useCallback, useEffect } from 'react'
+import PrintError from '../Errors/Error'
 import LinearIndeterminate from '../appHome/ProgressBar'
 import AxiosInstanse from '../Axios'
 import DialogDeviceDetail from './Devices/DialogDeviceDetail'
 import { Typography, Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import PropTypes from 'prop-types'
 
 export default function DetailPanel({ row, link }) {
   const [history, setHistory] = useState([])
@@ -26,65 +28,59 @@ export default function DetailPanel({ row, link }) {
     GetData()
   }, [])
 
-  if (isLoading)
-    return (
-      <Box mt={1.25}>
-        <LinearIndeterminate />
-      </Box>
-    )
-  if (error) return <PrintError error={error} />
-  if (history.length === 0)
-    return (
-      <Typography sx={{ mt: 1.5, pt: 0.5, pb: 0.5, textAlign: 'center' }} component={Paper}>
-        История не обнаружена
-      </Typography>
-    )
-
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Название</TableCell>
-            <TableCell>Количество</TableCell>
-            <TableCell>Дата последней установки</TableCell>
-            <TableCell>Статус</TableCell>
-            <TableCell>Примечание</TableCell>
-            <TableCell>Пользователь</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {history.map((item, index) => (
-            <TableRow key={index}>
-              <TableCell>{item.stock_model}</TableCell>
-              <TableCell>{item.quantity}</TableCell>
-              <TableCell>{item.dateInstall}</TableCell>
-              <TableCell>
-                <Box>
-                  {
-                    // eslint-disable-next-line react/prop-types
-                    item.device ? (
-                      <DialogDeviceDetail
-                        // eslint-disable-next-line react/prop-types
-                        status={item.status}
-                        // eslint-disable-next-line react/prop-types
-                        device={item.device}
-                        // eslint-disable-next-line react/prop-types
-                        id={item.deviceId}
-                      />
-                    ) : (
-                      // eslint-disable-next-line react/prop-types
-                      item.status
-                    )
-                  }
-                </Box>
-              </TableCell>
-              <TableCell>{item.note}</TableCell>
-              <TableCell>{item.user}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      {isLoading ? (
+        <Box mt={1.25}>
+          <LinearIndeterminate />
+        </Box>
+      ) : error ? (
+        <PrintError error={error} />
+      ) : history.length === 0 ? (
+        <Typography sx={{ mt: 1.5, pt: 0.5, pb: 0.5, textAlign: 'center' }} component={Paper}>
+          История не обнаружена
+        </Typography>
+      ) : (
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Название</TableCell>
+                <TableCell>Количество</TableCell>
+                <TableCell>Дата последней установки</TableCell>
+                <TableCell>Статус</TableCell>
+                <TableCell>Примечание</TableCell>
+                <TableCell>Пользователь</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {history.map((item, index) => (
+                <TableRow key={index}>
+                  <TableCell>{item.stock_model}</TableCell>
+                  <TableCell>{item.quantity}</TableCell>
+                  <TableCell>{item.dateInstall}</TableCell>
+                  <TableCell>
+                    <Box>
+                      {item.device ? (
+                        <DialogDeviceDetail status={item.status} device={item.device} id={item.deviceId} />
+                      ) : (
+                        item.status
+                      )}
+                    </Box>
+                  </TableCell>
+                  <TableCell>{item.note}</TableCell>
+                  <TableCell>{item.user}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+    </>
   )
+}
+
+DetailPanel.propTypes = {
+  row: PropTypes.uuid,
+  link: PropTypes.str,
 }
