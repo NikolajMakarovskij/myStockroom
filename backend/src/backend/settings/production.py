9f64936fa854dc074d41ffed333b46f64a11bbaf
NikolajMakarovskij <adrameleh666@gmail.com>
2025-04-25 16:56:16 +0500

    delete debug toolbar

--- backend/src/backend/settings/production.py
+++ backend/src/backend/settings/production.py
@@ -1,5 +1,4 @@
 import os
-import socket
 from datetime import timedelta
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
@@ -45,39 +44,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-
-# start debug toolbar
-
-DEBUG_TOOLBAR_PANELS = [
-    "debug_toolbar.panels.history.HistoryPanel",
-    "debug_toolbar.panels.versions.VersionsPanel",
-    "debug_toolbar.panels.timer.TimerPanel",
-    "debug_toolbar.panels.settings.SettingsPanel",
-    "debug_toolbar.panels.headers.HeadersPanel",
-    "debug_toolbar.panels.request.RequestPanel",
-    "debug_toolbar.panels.sql.SQLPanel",
-    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
-    "debug_toolbar.panels.templates.TemplatesPanel",
-    "debug_toolbar.panels.cache.CachePanel",
-    "debug_toolbar.panels.signals.SignalsPanel",
-    "debug_toolbar.panels.logging.LoggingPanel",
-    "debug_toolbar.panels.redirects.RedirectsPanel",
-    "debug_toolbar.panels.profiling.ProfilingPanel",
-]
-
-
-def show_toolbar(request):
-    return False
-
-
-DEBUG_TOOLBAR_CONFIG = {
-    "SHOW_TOOLBAR_CALLBACK": show_toolbar,
-}
-
-hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-INTERNAL_IPS = [ip[:-1] + "1" for ip in ips] + ["127.0.0.1", "x.x.x.x", "x.x.x.x"]
-
-# end debug toolbar
 
 # start celery
 
