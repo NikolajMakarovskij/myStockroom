80077dc32e42b43ae714366962ddc4fa9fb3bf91
NikolajMakarovskij <adrameleh666@gmail.com>
2025-04-29 09:35:18 +0500

    refactor cards

--- frontend/src/components/appStock/IndexStock.jsx
+++ frontend/src/components/appStock/IndexStock.jsx
@@ -1,7 +1,4 @@
 import * as React from 'react'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { Card, CardActionArea, CardContent, CardMedia, Grid, Typography } from '@mui/material'
-import { Link } from 'react-router-dom'
 import ListStockDevices from './Devices/ListStockDevices.jsx'
 import ListStockConsumables from './Consumables/ListStockConsumables.jsx'
 import ListHistoryConsumables from './Consumables/ListHistoryConsumables.jsx'
@@ -9,12 +6,7 @@ import ListConsumptionConsumables from './Consumables/ListConsumptionConsumables
 import ListStockAccessories from './Accessories/ListStockAccessories.jsx'
 import ListHistoryAccessories from './Accessories/ListHistoryAccessories.jsx'
 import ListConsumptionAccessories from './Accessories/ListConsumptionAccessories.jsx'
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-})
+import GridCards from '../Surface/GridCards.jsx'
 
 const StockContent = [
   {
@@ -70,38 +62,7 @@ const StockContent = [
 export { StockContent }
 
 const IndexStock = () => {
-  return (
-    <Grid container style={{ marginLeft: '50px' }}>
-      <ThemeProvider theme={darkTheme}>
-        {StockContent.map((item) => (
-          <Grid key={item.key} size={{ xs: 1, sm: 6, md: 4 }} style={{ marginBottom: '80px' }}>
-            <Card
-              sx={{ minWidth: 200, maxWidth: 200, minHeight: 200, maxHeight: 200, borderRadius: 5, display: 'flex' }}
-            >
-              <CardActionArea
-                component={Link}
-                to={item.url_path}
-                element={item.url_name}
-                sx={{ display: 'flex', flexDirection: 'column' }}
-              >
-                <CardContent>
-                  <Typography gutterBottom component='div'>
-                    {item.title}
-                  </Typography>
-                </CardContent>
-                <CardMedia
-                  sx={{ border: 0, minWidth: 120, maxWidth: 120, minHeight: 120, maxHeight: 120 }}
-                  component='img'
-                  image={item.image}
-                  alt={item.title}
-                />
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
-      </ThemeProvider>
-    </Grid>
-  )
+  return <GridCards content={StockContent} />
 }
 
 export default IndexStock
--- frontend/src/components/appWorkplace/IndexWorkplace.jsx
+++ frontend/src/components/appWorkplace/IndexWorkplace.jsx
@@ -1,67 +1,26 @@
 import * as React from 'react'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { Card, CardActionArea, CardContent, CardMedia, Grid, Typography } from '@mui/material'
-import { Link } from 'react-router-dom'
-import ListWorkplace from './Workplaces/ListWorkplace'
-import ListRoom from './Rooms/ListRoom'
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-})
+import GridCards from '../Surface/GridCards'
 
 const WorkplaceContent = [
   {
     key: 'workplace',
     title: 'Рабочие места',
     url_path: '/workplace/list',
-    url_name: ListWorkplace,
+    url_name: 'ListWorkplace',
     image: 'http://localhost/static/images/workplace.svg',
   },
   {
     key: 'room',
     title: 'Помещение',
     url_path: '/room/list',
-    url_name: ListRoom,
+    url_name: 'ListRoom',
     image: 'http://localhost/static/images/room.svg',
   },
 ]
 export { WorkplaceContent }
 
 const IndexWorkplace = () => {
-  return (
-    <Grid container style={{ marginLeft: '50px' }}>
-      <ThemeProvider theme={darkTheme}>
-        {WorkplaceContent.map((item) => (
-          <Grid key={item.key} size={{ xs: 12, sm: 6, md: 4 }} style={{ marginBottom: '80px' }}>
-            <Card
-              sx={{ minWidth: 200, maxWidth: 200, minHeight: 200, maxHeight: 200, borderRadius: 5, display: 'flex' }}
-            >
-              <CardActionArea
-                component={Link}
-                to={item.url_path}
-                element={item.url_name}
-                sx={{ display: 'flex', flexDirection: 'column' }}
-              >
-                <CardContent>
-                  <Typography gutterBottom component='div'>
-                    {item.title}
-                  </Typography>
-                </CardContent>
-                <CardMedia
-                  sx={{ border: 0, minWidth: 120, maxWidth: 120, minHeight: 120, maxHeight: 120 }}
-                  component='img'
-                  image={item.image}
-                  alt={item.title}
-                />
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
-      </ThemeProvider>
-    </Grid>
-  )
+  return <GridCards content={WorkplaceContent} />
 }
 
 export default IndexWorkplace
