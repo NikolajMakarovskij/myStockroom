eb50715f64798f0678ea1899ef651a5f56f90a58
NikolajMakarovskij <adrameleh666@gmail.com>
2025-05-16 16:28:19 +0500

    added  counterparty docs

--- backend/src/counterparty/serializers.py
+++ backend/src/counterparty/serializers.py
@@ -4,9 +4,17 @@ from .models import Manufacturer
 
 
 class ManufacturerSerializer(serializers.ModelSerializer[Manufacturer]):
-    queryset = Manufacturer.objects.all()
+    """_ManufacturerSerializer_ Serialize Manufacturer Model to JSON"""
 
     class Meta:
+        """_Class returns JSON of Manufacturer model_
+
+        Returns:
+            model (Manufacturer):
+            fields (list[str]): _returns fields of model in form_
+            extra_kwargs (dict[str,list[str]): _returns settings of fields_
+        """
+
         model = Manufacturer
         fields = "__all__"
         extra_kwargs = {"id": {"read_only": True}}
