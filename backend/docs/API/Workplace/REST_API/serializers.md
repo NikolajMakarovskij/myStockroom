bae19ca0926cc2d1741e9e807a3fa4f7bda2ec90
NikolajMakarovskij <adrameleh666@gmail.com>
2025-05-22 16:55:22 +0500

    added docs in workplace app

--- backend/docs/API/Workplace/REST_API/serializers.md
+++
@@ -1,6 +0,0 @@
-# ***Serializers***
-
-## Workplace
-
-### WorkplaceSerializer
-#### :::src.workplace.serializers
--- backend/src/workplace/serializers.py
+++ backend/src/workplace/serializers.py
@@ -4,38 +4,80 @@ from .models import Room, Workplace
 
 
 class WorkplaceSerializer(serializers.ModelSerializer[Workplace]):
-    queryset = Workplace.objects.all()
+    """_WorkplaceSerializer_ Serialize Workplace Model to JSON"""
 
     class Meta:
+        """_Class returns JSON of Workplace model_
+
+        Returns:
+            model (Workplace):
+            fields (list[str]): _returns fields of model in form_
+            extra_kwargs (dict[str,list[str]): _returns settings of fields_
+        """
+
         model = Workplace
         fields = "__all__"
         extra_kwargs = {"id": {"read_only": True}}
 
 
 class RoomModelSerializer(serializers.ModelSerializer[Room]):
-    queryset = Room.objects.all()
+    """_RoomModelSerializer_ Serialize Room Model with extended fields JSON
+
+    Other parameters:
+        workplace (Workplace): _serialize Workplace model_
+    """
+
     workplace = WorkplaceSerializer(many=True, read_only=True)
 
     class Meta:
+        """_Class returns JSON of Room model_
+
+        Returns:
+            model (Room):
+            fields (list[str]): _returns fields of model in form_
+            extra_kwargs (dict[str,list[str]): _returns settings of fields_
+        """
+
         model = Room
         fields = "__all__"
         extra_kwargs = {"id": {"read_only": True}}
 
 
 class RoomSerializer(serializers.ModelSerializer[Room]):
-    queryset = Room.objects.all()
+    """_RoomSerializer_ Serialize Room Model to JSON"""
 
     class Meta:
+        """_Class returns JSON of Room model_
+
+        Returns:
+            model (Room):
+            fields (list[str]): _returns fields of model in form_
+            extra_kwargs (dict[str,list[str]): _returns settings of fields_
+        """
+
         model = Room
         fields = "__all__"
         extra_kwargs = {"id": {"read_only": True}}
 
 
 class WorkplaceListSerializer(serializers.ModelSerializer[Workplace]):
-    queryset = Workplace.objects.all()
+    """_WorkplaceListSerializer_ Serialize Room Model with extended fields JSON
+
+    Other parameters:
+        room (Room: _serialize Room model_
+    """
+
     room = RoomSerializer(read_only=True)
 
     class Meta:
+        """_Class returns JSON of Workplace model_
+
+        Returns:
+            model (Workplace):
+            fields (list[str]): _returns fields of model in form_
+            extra_kwargs (dict[str,list[str]): _returns settings of fields_
+        """
+
         model = Workplace
         fields = "__all__"
         extra_kwargs = {"id": {"read_only": True}}
