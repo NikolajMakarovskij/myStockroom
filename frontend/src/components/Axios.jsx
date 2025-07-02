fc37263e1d213627bf42435334c1264ca89dfb62
NikolajMakarovskij <adrameleh666@gmail.com>
2025-07-02 17:48:59 +0500

    added react consts ro env

--- frontend/src/components/Axios.jsx
+++ frontend/src/components/Axios.jsx
@@ -1,12 +1,22 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost/api/'
+function getApiUrl() {
+  const DEBUG = import.meta.env.REACT_APP_DEBUG
+  let baseUrl
+  if (DEBUG) {
+    baseUrl = 'http://localhost/api/'
+  } else {
+    baseUrl = import.meta.env.REACT_APP_API_URL
+  }
+  return baseUrl
+}
+
 axios.defaults.xsrfCookieName = 'csrftoken'
 axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 axios.defaults.withCredentials = true
 
 const AxiosInstanse = axios.create({
-  baseURL: baseUrl,
+  baseURL: getApiUrl(),
   timeout: 5000,
   headers: {
     'Content-type': 'Application/json',
